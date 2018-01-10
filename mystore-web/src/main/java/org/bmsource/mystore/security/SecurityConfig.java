5f628ce2914b76e62b7eeb5a4b47d932c5ffcd39
Marek Balla <marecica2@gmail.com>
2018-01-10 14:50:25 +0100

    oauth partially working

--- mystore-web/src/main/java/org/bmsource/mystore/security/SecurityConfig.java
+++ mystore-web/src/main/java/org/bmsource/mystore/security/SecurityConfig.java
@@ -1,67 +1,66 @@
 package org.bmsource.mystore.security;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+//@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.csrf().disable()
-		.cors().configurationSource(new CorsConfigurationSource() {
-			
-			@Override
-			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-		        CorsConfiguration config = new CorsConfiguration();
-		        config.setAllowCredentials(true);
-		        config.addAllowedOrigin("*");
-		        config.addAllowedHeader("*");
-		        config.addAllowedMethod("OPTIONS");
-		        config.addAllowedMethod("GET");
-		        config.addAllowedMethod("POST");
-		        config.addAllowedMethod("PUT");
-		        config.addAllowedMethod("DELETE");
-				return config;
-			}
-		})
-		.and().authorizeRequests()
-        .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-        .anyRequest().authenticated()
-        
-		.and().formLogin().permitAll()
-		.and().logout().invalidateHttpSession(true).clearAuthentication(true).permitAll()
-		.and().httpBasic();
-	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(customAuthenticationProvider());
-	}
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() 
+      throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-	private DaoAuthenticationProvider customAuthenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(encoder());
-		return authProvider;
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//		//.csrf().disable()
+//		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//		.and().cors()
+//		.and().authorizeRequests()
+//        .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+//        .anyRequest().authenticated()
+//		.and().logout().invalidateHttpSession(true).clearAuthentication(true).permitAll();
+//	}
+//
+//	/**
+//	 * corsConfigurationSource injected per default
+//	 */
+//	@Bean
+//	CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+//		configuration.setAllowedHeaders(Arrays.asList("*"));
+//		configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "DELETE", "PUT"));
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
+	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.authenticationProvider(customAuthenticationProvider());
+//	}
+//
+//	private DaoAuthenticationProvider customAuthenticationProvider() {
+//		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//		authProvider.setUserDetailsService(userDetailsService);
+//		authProvider.setPasswordEncoder(encoder());
+//		return authProvider;
+//	}
 	
 	@Bean
 	public PasswordEncoder encoder() {
--- mystore-web/src/main/java/org/bmsource/mystore/user/rest/UserController.java
+++ mystore-web/src/main/java/org/bmsource/mystore/user/rest/UserController.java
@@ -1,19 +1,29 @@
 package org.bmsource.mystore.user.rest;
 
+import java.util.List;
+
 import org.bmsource.mystore.user.User;
 import org.bmsource.mystore.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@PreAuthorize("#oauth2.hasScope('read')")
 public class UserController {
 
     @Autowired
     UserRepository repository;
 
+    @PreAuthorize("#oauth2.hasScope('read')")
     @GetMapping
     public List<User> getUsers() {
 		return repository.findAll();
